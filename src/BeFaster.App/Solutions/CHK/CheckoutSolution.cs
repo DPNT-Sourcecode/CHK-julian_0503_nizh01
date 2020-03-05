2508dd6eeb91033fd027ab518c0a7e617bb67c74
sbx_user1051 <sbx_user1051@169.254.23.101>
2020-03-05 10:14:52 +0000

    Thu Mar 05 10:14:52 UTC 2020

--- src/BeFaster.App/Solutions/CHK/CheckoutSolution.cs
+++ src/BeFaster.App/Solutions/CHK/CheckoutSolution.cs
@@ -1,4 +1,6 @@
 ﻿using BeFaster.Runner.Exceptions;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace BeFaster.App.Solutions.CHK
 {
@@ -6,7 +8,28 @@ namespace BeFaster.App.Solutions.CHK
     {
         public static int ComputePrice(string skus)
         {
-            throw new SolutionNotImplementedException();
+            int checkoutAmount = 0;
+            if (string.IsNullOrEmpty(skus))
+            {
+                return 0;
+            }
+            if (!Regex.IsMatch(skus, @"^[A-D]+$"))
+            {
+                return -1;
+            }
+            int A_count = skus.Count(v => v == 'A');
+            int B_count = skus.Count(v => v == 'B');
+            int C_count = skus.Count(v => v == 'C');
+            int D_count = skus.Count(v => v == 'D');
+            checkoutAmount = checkoutAmount + ((A_count % 3) * 50);
+            checkoutAmount = checkoutAmount + ((A_count / 3) * 130);
+            checkoutAmount = checkoutAmount + ((B_count % 2) * 30);
+            checkoutAmount = checkoutAmount + ((B_count / 2) * 45);
+            checkoutAmount = checkoutAmount + (C_count * 20);
+            checkoutAmount = checkoutAmount + (D_count * 15);
+            return checkoutAmount;
         }
     }
 }
+
+
